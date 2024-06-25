0d3c53db6691f8144f647c9f9cfd7154a41572ed
PurnaSutha <purnasutha@gmail.com>
2024-06-26 00:33:33 +0530

    Add files via upload
    assignment week 1-day2

---
+++ day2assignment/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package week1.day2assignment;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) 
+	{
+         int range=8;//Input range
+         int a=0,b=1;//Initializing  the variables
+         System.out.println("Fibonacci Series is");
+         for (int i=0;i<range;i++)//for loop 
+         {
+        	 System.out.println(a);//Print statement to print the Fibonacci series
+        	 int c=a+b;
+        	 a=b;
+        	 b=c;
+         }
+	}
+
+}
